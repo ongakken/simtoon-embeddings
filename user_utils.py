5c3df430e20bf999d424f4e86cdf71e9f3739cda
Simon Slamka <simon.slamka@ongakken.com>
2024-01-11 13:26:43 +0100

    trying to debug the issue with maxfeatures

--- user_utils.py
+++ user_utils.py
@@ -85,7 +85,7 @@ class UserUtils:
     def train_isolation_forest_for_user(self, embs: Tensor) -> IsolationForest:
         forest = IsolationForest(random_state=69, n_estimators=1000, n_jobs=-1).fit(embs.cpu().numpy())
         logging.info(f"Trained IsolationForest for user.")
-        logging.debug(f"Trained IsolationForest for user with {len(embs)} embeddings.\nmaxSamples: {forest.max_samples}\ncontamination: {forest.contamination}\nnEstimators: {forest.n_estimators}\nmaxFeatures: {forest.max_features}\nnJobs: {forest.n_jobs}")
+        logging.debug(f"Trained IsolationForest for user with {len(embs)} embeddings.\nmaxSamples: {forest.max_samples}\ncontamination: {forest.contamination}\nnEstimators: {forest.n_estimators}\nmaxFeatures: {forest.max_features}\nnJobs: {forest.n_jobs}\n{forest.n_features_in_}")
         return forest
 
     def plot_and_measure_spread(self, embs1: Tuple[Tensor, str], embs2: Tuple[Tensor, str], reduceFunc, title: str) -> None:
