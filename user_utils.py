ffb29d91d444445208405bf7586ef27aaf144674
Simon Slamka <simon.slamka@ongakken.com>
2024-01-12 11:50:05 +0100

    forest - change contamination back to auto

--- user_utils.py
+++ user_utils.py
@@ -84,7 +84,7 @@ class UserUtils:
         return ranges, stds, hullVol, hull
 
     def train_isolation_forest_for_user(self, embs: Tensor) -> IsolationForest:
-        forest = IsolationForest(random_state=69, contamination=0.01, n_estimators=1000, n_jobs=-1, max_features=768, max_samples=embs.shape[0]).fit(embs.cpu().numpy())
+        forest = IsolationForest(random_state=69, contamination="auto", n_estimators=1000, n_jobs=-1, max_features=768, max_samples=embs.shape[0]).fit(embs.cpu().numpy())
         logging.info(f"Trained IsolationForest for user.")
         logging.debug(f"Trained IsolationForest for user with {len(embs)} embeddings.\nmaxSamples: {forest.max_samples_}\ncontamination: {forest.contamination}\nnEstimators: {forest.n_estimators}\nmaxFeatures: {forest.max_features}\nnJobs: {forest.n_jobs}\nn_features_in: {forest.n_features_in_}")
         return forest
